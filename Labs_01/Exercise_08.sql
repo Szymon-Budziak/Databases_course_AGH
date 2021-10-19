daad3534bd987502b296b52d547f6435ef886318
szymonbudziak@gmail.com <szymonbudziak@gmail.com>
2021-10-19 22:37:00 +0200

    Labs 1 - northwind database and select command

--- Labs_01/Exercise_08.sql
+++ Labs_01/Exercise_08.sql
@@ -1,10 +1,19 @@
 -- 1. Napisz polecenie, które oblicza wartość każdej pozycji zamówienia o numerze 10250
-SELECT ProductID, UnitPrice * Quantity AS 'Value'
+SELECT ProductID, UnitPrice * Quantity * (1 - Discount) AS 'Value'
 FROM [Order Details]
 WHERE OrderID = 10250
 
 -- 2. Napisz polecenie które dla każdego dostawcy (supplier) pokaże pojedynczą kolumnę
 -- zawierającą nr telefonu i nr faksu w formacie (numer telefonu i faksu mają być
 -- oddzielone przecinkiem)
-SELECT Phone + ', ' + Fax AS 'Contact'
+-- first solution
+SELECT CompanyName, Phone + ISNULL(', ' + Fax, '') AS 'Contact'
+FROM Suppliers
+
+-- second solution
+SELECT CompanyName,
+       CASE
+           WHEN Fax IS NOT NULL THEN Phone + ', ' + Fax
+           ELSE Phone
+           END AS 'Contact'
 FROM Suppliers
