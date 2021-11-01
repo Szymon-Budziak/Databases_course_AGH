fb368b32b0e46ab7ab57dce594026f27ceb2bbf4
szymonbudziak@gmail.com <szymonbudziak@gmail.com>
2021-11-01 11:46:07 +0100

    Labs 4

---
+++ Labs_04/Exercise_04.sql
@@ -0,0 +1,9 @@
+-- 1. Dla każdego pracownika podaj liczbę obsługiwanych przez niego zamówień z podziałem na lata i miesiące
+SELECT EmployeeID, COUNT(*), YEAR(OrderDate) AS 'Year', MONTH(OrderDate) AS 'Month', COUNT(*) AS 'Number of orders'
+FROM Orders
+GROUP BY EmployeeID, YEAR(OrderDate), MONTH(OrderDate)
+
+-- 2. Dla każdej kategorii podaj maksymalną i minimalną cenę produktu w tej kategorii
+SELECT CategoryID, MAX(UnitPrice) AS 'Max price', MIN(UnitPrice) AS 'Min price'
+FROM Products
+GROUP BY CategoryID
