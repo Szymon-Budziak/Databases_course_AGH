f146dc0c1f1d84de93d253375aa0979eee7edc34
szymonbudziak@gmail.com <szymonbudziak@gmail.com>
2021-11-29 22:26:07 +0100

    Labs 4 update

--- Labs_04/Exercise_03.sql
+++ Labs_04/Exercise_03.sql
@@ -1,17 +1,22 @@
 -- 1. Dla każdego pracownika podaj liczbę obsługiwanych przez niego zamówień
-SELECT EmployeeID, COUNT(*)
+SELECT EmployeeID,
+       COUNT(*)
 FROM Orders
 GROUP BY EmployeeID
 
 -- 2. Dla każdego spedytora/przewoźnika podaj wartość "opłata za przesyłkę" przewożonych przez niego zamówień
-SELECT ShipVia, OrderID, SUM(Freight) AS 'opłata za przesyłkę'
+SELECT ShipVia,
+       OrderID,
+       SUM(Freight) AS 'freight_price'
 FROM Orders
 GROUP BY ShipVia, OrderID
 WITH ROLLUP
 
 -- 3. Dla każdego spedytora/przewoźnika podaj wartość "opłata za przesyłkę" przewożonych przez niego zamówień
--- w latach o 1996 do 1997
-SELECT ShipVia, OrderID, SUM(Freight) AS 'opłata za przesyłkę'
+-- w latach od 1996 do 1997
+SELECT ShipVia,
+       OrderID,
+       SUM(Freight) AS 'freight_price'
 FROM Orders
 WHERE YEAR(ShippedDate) BETWEEN 1996 AND 1997
 GROUP BY ShipVia, OrderID
